2c8c86a407664afc89988c1794607808ca57f27f
harsha <harsha200124@gmail.com>
2023-12-16 23:39:52 +0530

    partial part A code

---
+++ script/index.js
@@ -0,0 +1,16 @@
+function SignInmodel(){
+    document.getElementById('modal').style.display="block";
+}
+function SignUPmodel(){
+    document.getElementById('SignInmodel').style.display='block';
+}
+function close(){
+    document.getElementById('close').style.display='none';
+    
+}
+
+
+  
+
+
+
