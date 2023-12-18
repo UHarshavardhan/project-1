dd6d5996c810e005a513081d7bad6fa6cb226698
harsha <harsha200124@gmail.com>
2023-12-18 12:31:23 +0530

    added new files

--- script/postslist.js
+++ script/postslist.js
@@ -1,3 +1,30 @@
-function openDeleteModal(){
-    document.getElementById('delete1').style.display="block";
+function openDeleteModal(id) {
+    // Store the post ID in a variable
+    var postId = id;
+
+    // Display the delete confirmation modal
+    document.getElementById('delete1').style.display = "block";
+
+    // Set up the event listener for the "yes" button
+    document.getElementById('yes').addEventListener('click', function () {
+        // Call a function to delete the post
+        deletePost(postId);
+
+        // Hide the delete confirmation modal
+        document.getElementById('delete1').style.display = "none";
+    });
+
+    // Set up the event listener for the "no" button
+    document.getElementById('No').addEventListener('click', function () {
+        // Hide the delete confirmation modal
+        document.getElementById('delete1').style.display = "none";
+    });
+}
+
+function deletePost(postId) {
+    // Remove the post with the specified ID from the DOM
+    var postElement = document.getElementById(postId);
+    if (postElement) {
+        postElement.parentNode.removeChild(postElement);
+    }
 }
