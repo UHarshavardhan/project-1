4a43de7fb49319b0985f99cd6197df1a5a5e529e
harsha <harsha200124@gmail.com>
2023-12-18 20:31:41 +0530

    added new files

--- script/index.js
+++ script/index.js
@@ -1,15 +1,22 @@
-function SignInmodel(){
-    document.getElementById('inmodal').style.display="block";
-}
-function SignUPmodel(){
-    document.getElementById('upmodal').style.display="block";
+function SignInmodel() {
+    document.getElementById('inmodal').style.display = "block";
 }
 
-function pen(){
-    document.getElementById('pen').style.display="block";
+function SignUPmodel() {
+    document.getElementById('upmodal').style.display = "block";
+}
+function pen() {
+    document.getElementById('pen').style.display = "block";
+}
+function close()  {
+
+     document.getElementById('upmodal').style.display="none";
+     document.getElementById('inmodal').style.display="none";
+     document.getElementById('pen').style.display="none";
+
 }
 
-  
-
-
+document.getElementById("lol").addEventListener('click',close);
+document.getElementById("lo").addEventListener('click',close);
+document.getElementById("pen1").addEventListener('click',close);
 
